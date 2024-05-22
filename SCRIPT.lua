97d5363ef8ad882461f3be6b7db8f7266b316110
ASSISTASD <gty7215@gmail.com>
2024-05-22 19:35:48 +0100

    Update SCRIPT.lua

--- SCRIPT.lua
+++ SCRIPT.lua
@@ -19,6 +19,34 @@ end
 
 
 ------------------ fun
+require(game.ReplicatedStorage.Util.CameraShaker):Stop()
+
+xShadowFastAttackx = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
+
+xShadowx = debug.getupvalues(xShadowFastAttackx)[2]
+
+spawn(function()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if _G.FastAttack then
+            if typeof(xShadowx) == "table" then
+                pcall(function()
+                    xShadowx.activeController.timeToNextAttack = (math.huge^math.huge^math.huge)
+                    xShadowx.activeController.timeToNextAttack = 0
+                    xShadowx.activeController.hitboxMagnitude = 200
+                    xShadowx.activeController.active = false
+                    xShadowx.activeController.timeToNextBlock = 0
+                    xShadowx.activeController.focusStart = 0
+                    xShadowx.activeController.increment = 4
+                    xShadowx.activeController.blocking = false
+                    xShadowx.activeController.attacking = false
+                    xShadowx.activeController.humanoid.AutoRotate = 50
+                end)
+            end
+        end
+    end)
+end)
+
+
 function EquipWeaponSword()
 	pcall(function()
 		for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
@@ -1208,3 +1236,16 @@ AFTAP:AddButton({
   	end    
 })
 
+local STAP = Window:MakeTab({
+	Name = "AUTO FARM",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+STAP:AddToggle({
+	Name = "FAST ATTACK",
+	Default = _G.Auto_Farm_Bone,
+	Callback = function(Value)
+		_G.FastAttack = Value
+	end    
+})
